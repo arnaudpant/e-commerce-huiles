267adfba58b81cce03773dfd00f20506cb933827
macbook kal <arnaud.pantenier@gmail.com>
2024-08-02 10:47:54 +0200

    ADD Page products

---
+++ app/dashboard/products/page.tsx
@@ -0,0 +1,82 @@
+import { Button } from "@/app/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/app/components/ui/card";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/app/components/ui/dropdown-menu";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/app/components/ui/table";
+import { MoreHorizontal, PlusCircle, UserIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function Products() {
+    return (
+        <>
+            <div className="flex items-center justify-end">
+                <Button asChild className="flex items-center gap-2">
+                    <Link href="/dashboard/products/create">
+                        <PlusCircle className="h-3.5 w-3.5" />
+                        <span>Ajouter produit</span>
+                    </Link>
+                </Button>
+            </div>
+            <Card className="mt-5">
+                <CardHeader>
+                    <CardTitle>Produits</CardTitle>
+                    <CardDescription>Gestion des produits</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>
+                                    Image
+                                </TableHead>
+                                <TableHead>Nom</TableHead>
+                                <TableHead>Status</TableHead>
+                                <TableHead>Prix</TableHead>
+                                <TableHead>Date</TableHead>
+                                <TableHead className="text-end">Actions</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>
+                                    <UserIcon w-16 h-16/>
+                                </TableCell>
+                                <TableCell>Huile vierge de noix</TableCell>
+                                <TableCell>En stock</TableCell>
+                                <TableCell>25 €</TableCell>
+                                <TableCell>10/02/2024</TableCell>
+                                <TableCell className="text-end">
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button size='icon' variant='ghost'>
+                                                <MoreHorizontal className="h-4 w-4" />
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>Action</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem>Modifier</DropdownMenuItem>
+                                            <DropdownMenuItem>Supprimer</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </CardContent>
+            </Card>
+        </>
+    );
+}
