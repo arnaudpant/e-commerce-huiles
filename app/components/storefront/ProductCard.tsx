86df3b4f5fc64253ff087c8d7b6e1ca848de199b
macbook kal <arnaud.pantenier@gmail.com>
2024-08-19 21:38:14 +0200

    STYLE Modif ProductCard

--- app/components/storefront/ProductCard.tsx
+++ app/components/storefront/ProductCard.tsx
@@ -29,7 +29,7 @@ type Props = {
 
 export function ProductCard({ item }: Props) {
     return (
-        <div className="rounded-lg">
+        <div className="rounded-lg min-h-[546px] relative">
             <Carousel>
                 <CarouselContent className="w-full mx-auto">
                     {item.images.map((imgUrl, index) => (
@@ -53,9 +53,9 @@ export function ProductCard({ item }: Props) {
                 )}
             </Carousel>
 
-            <div className="flex justify-between items-start mt-2">
+            <div className="flex justify-between items-start mt-2 h-20">
                 <h1 className="font-semibold text-xl">{item.name}</h1>
-                <div className="flex flex-col gap-y-1 w-[130px] pl-2">
+                <div className="flex flex-col gap-y-1 min-w-[110px] pl-2">
                     {item.price50 !== 0 && (
                         <h3 className="inline-flex items-center justify-center rounded-md bg-primary/10 px-2 py-1 text-xs font-medium text-primary ring-1 ring-inset ring-primary/10">
                             {item.price50}€ / 50ml
@@ -76,9 +76,12 @@ export function ProductCard({ item }: Props) {
             <p className="text-gray-600 text-sm mt-2 line-clamp-3">
                 {item.description}
             </p>
+            <div className="absolute w-full bottom-0">
             <Button asChild className="w-full mt-5">
                 <Link href={`/product/${item.id}`}>Commander</Link>
             </Button>
+
+            </div>
         </div>
     );
 }
